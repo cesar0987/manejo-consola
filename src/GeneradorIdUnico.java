cf7f402102bc49a498f94b8273ddce987a0e1519
cesar0987 <cesarbenitez46@gmail.com>
2024-09-16 21:13:14 -0400

    Generador de ID unico

--- src/GeneradorIdUnico.java
+++ src/GeneradorIdUnico.java
@@ -1,2 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GeneradorIdUnico {
+    public static void main(String[] args) {
+        System.out.println("Generador de ID Unico");
+        var consola = new Scanner(System.in);
+        System.out.print("Cual es tu nombre: ");
+        var nombre = consola.nextLine();
+        System.out.print("Cual es tu apellido: ");
+        var apellido = consola.nextLine();
+        System.out.print("Cual es tu año de nacimiento:");
+        var añoNacimiento= Integer.parseInt(consola.nextLine());
+        var random = new Random();
+
+
+    }
 }
